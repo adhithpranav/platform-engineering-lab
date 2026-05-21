2b5f5115059fe382f0932772a94dcf46d320af6f
Ubuntu <ubuntu@ip-10-0-1-5.ap-south-1.compute.internal>
2026-05-21 12:49:22 +0000

    Add NGINX reverse proxy and route frontend/backend traffic

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,32 +1,18 @@
-"use client";
+async function getMessage() {
+  const res = await fetch("http://backend:8000", {
+    cache: "no-store",
+  });
 
-import { useEffect, useState } from "react";
+  return res.json();
+}
 
-export default function Home() {
-  const [message, setMessage] = useState("INITIAL STATE");
-
-  useEffect(() => {
-    console.log("USE EFFECT STARTED");
-
-    fetch("http://52.66.183.220:8000")
-      .then((res) => {
-        console.log("FETCH RESPONSE", res);
-        return res.json();
-      })
-      .then((data) => {
-        console.log("FETCH DATA", data);
-        setMessage(data.message);
-      })
-      .catch((err) => {
-        console.error("FETCH ERROR", err);
-        setMessage("FETCH FAILED");
-      });
-  }, []);
+export default async function Home() {
+  const data = await getMessage();
 
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="text-5xl font-bold">
-        {message}
+        {data.message}
       </div>
     </main>
   );
